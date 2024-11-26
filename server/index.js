0b9e9ab03504d3018a18a4d76c1403ac62870ee3
lev1c <dvauck2@gmail.com>
2024-11-27 02:46:23 +0400

    а

--- server/index.js
+++ server/index.js
@@ -1,39 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 const router = require('./routes/index');
-const firebaseAdmin = require('./firebase'); // Firebase инициализация
-
-const PORT = process.env.PORT || 8000;
+const firebaseAdmin = require('./firebase'); // Инициализация Firebase
 
 const app = express();
 
 app.use(cors());
 
+// Главный маршрут
 app.get('/', (req, res) => {
   res.send('Hello, Vercel!');
 });
 
-const server = http.createServer(app);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
+// Подключение дополнительных маршрутов
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb' }));
 app.use('/', router);
 
-const start = async () => {
-    try {
-        // Здесь больше не нужно подключаться к Sequelize
-        console.log('Firebase initialized');
-        server.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
-    } catch (e) {
-        console.log(e);
-    }
-};
-
-start();
+// Экспортируем как обработчик serverless функции для Vercel
+module.exports = app;
