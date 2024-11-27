ae9b621e9659a21781552052f56afd960915c7bc
lev1c <dvauck2@gmail.com>
2024-11-27 04:10:28 +0400

    ff

--- client/src/api/indexApi.js
+++ client/src/api/indexApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL
+    baseURL: "https://ogoneck.vercel.app/"
 })
 
 const $authHost = axios.create({
--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb' }));
-app.use('/api', router);
+app.use('/', router);
 
 
 const start = async () => {
