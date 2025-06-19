58ac0fc59f3e12b53f03a381136819a12ba28149
lev1c <dvauck2@gmail.com>
2025-06-19 07:05:25 +0400

    bew

--- client/src/api/indexApi.js
+++ client/src/api/indexApi.js
@@ -1,24 +1,21 @@
 import axios from "axios";
 
 const $linkHost = axios.create({
-    baseURL: "http://95.214.62.165:8000/api"
-})
+  baseURL: "http://217.114.9.95:8000/api",
+});
 
 const $authHost = axios.create({
-    baseURL: "http://localhost:8000/api/"
-})
+  baseURL: "http://localhost:8000/api/",
+});
 
-let $host = $linkHost
+let $host = $linkHost;
 
-const authInterceptor = config => {
-    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-    return config
-}
+const authInterceptor = (config) => {
+  config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+};
 
-$authHost.interceptors.request.use(authInterceptor)
-$linkHost.interceptors.request.use(authInterceptor)
+$authHost.interceptors.request.use(authInterceptor);
+$linkHost.interceptors.request.use(authInterceptor);
 
-export {
-    $host,
-    $authHost
-}
+export { $host, $authHost };
--- server/routes/documentRouter.js
+++ server/routes/documentRouter.js
@@ -4,34 +4,30 @@ const documentContoller = require("../controllers/documentController");
 const adminAuthMiddleware = require("../middleware/authMiddleware");
 const multer = require("multer");
 
-const crypto = require('crypto');
-
-
-
+const crypto = require("crypto");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/'); // Папка для сохранения
-    },
-    filename: (req, file, cb) => {
-      const uniqueId = crypto.randomBytes(8).toString('hex');
-      const extension = file.originalname.split('.').pop();
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Папка для сохранения
+  },
+  filename: (req, file, cb) => {
+    const uniqueId = crypto.randomBytes(8).toString("hex");
+    const extension = file.originalname.split(".").pop();
 
-      cb(null, `${uniqueId + "." + extension}`); // Уникальное имя файла
-    },
-  });
+    cb(null, `${uniqueId + "." + extension}`); // Уникальное имя файла
+  },
+});
 
 const upload = multer({ storage });
 
-router.get('/document/:fileName', documentContoller.getDocumentName);
+router.get("/document/:fileName", documentContoller.getDocumentName);
 
+router.get("/document-get", documentContoller.getAllDocument);
 
-router.get('/document-get', documentContoller.getAllDocument);
+router.post(
+  "/document-create",
+  upload.single("file"),
+  documentContoller.createDocument
+);
 
-router.post('/document-create', upload.single('file'), documentContoller.createDocument);
-
-router.delete('/document-delete/:id', documentContoller.deleteDocument);
-
-router.patch('/document-delete/:id', documentContoller.cnahgeNameDocument);
-
-module.exports = router
+module.exports = router;
